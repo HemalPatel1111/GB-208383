62f4f514a7f489d7795277f75f9430ac3defa2f1
HemalPatel1111 <38472887+HemalPatel1111@users.noreply.github.com>
2020-12-31 15:33:29 +0530

    lllll

--- src/Control/MoveController.gd
+++ src/Control/MoveController.gd
@@ -21,7 +21,7 @@ var LEFT  :float = 0
 var oldLEFT  :float = 0
 var onHit:bool=false
 
-var out:bool = false
+var out:bool = true
 
 func _process(delta):
 	var pos = $Pointer.get_position()
@@ -41,17 +41,17 @@ func _process(delta):
 		run = false
 		emit_signal("run", run)
 
-	if not run:
-		if(!onHit):
-			$Pointer.set_position(pos + deltaPos / 5)
-			ptr_pos = pos - ptr_pos_at_Rest
-			if ptr_pos.length() > 0.1:
-				emit_signal("Move", FRONT, LEFT)
-		else:
-			$Pointer.set_position(ptr_pos_at_Rest + ptr_pos)
+#	if not run:
+	if(out):
+		$Pointer.set_position(pos + deltaPos / 5)
+		ptr_pos = pos - ptr_pos_at_Rest
+		if ptr_pos.length() > 0.1:
+			emit_signal("Move", FRONT, LEFT)
 	else:
-		if(onHit):
-			$Pointer.set_position(ptr_pos_at_Rest + ptr_pos)
+		$Pointer.set_position(ptr_pos_at_Rest + ptr_pos)
+#	else:
+#		if(onHit):
+#			$Pointer.set_position(ptr_pos_at_Rest + ptr_pos)
 
 func _calc_Component():
 	oldFRONT = FRONT
@@ -65,43 +65,47 @@ func _on_gui(event):
 		emit_signal("onHit")
 
 func _on_Move_gui_input(event):
-	_on_gui(event)
+#	_on_gui(event)
+#
+#	if not run:
+#		if event is InputEventScreenTouch:
+#			if onHit :
+#				var dir = event.position - ptr_pos_at_Rest - ptr_size / 2
+#				ptr_pos = min(dir.length(),100) * dir.normalized()
+#				_calc_Component()
+#				emit_signal("Move", FRONT, LEFT)
+#
+#		elif event is InputEventScreenDrag:
+#			var dir = event.position - ptr_pos_at_Rest - ptr_size / 2
+#			ptr_pos = min(dir.length(),100) * dir.normalized()
+#
+#			_calc_Component()
+#			emit_signal("Move", FRONT, LEFT)
+#		else:
+#			FRONT = 0
+#			LEFT = 0
+	pass
 	
-	if not run:
-		if event is InputEventScreenTouch:
-			if onHit :
-				var dir = event.position - ptr_pos_at_Rest - ptr_size / 2
-				ptr_pos = min(dir.length(),100) * dir.normalized()
-				_calc_Component()
-				emit_signal("Move", FRONT, LEFT)
-		
-		elif event is InputEventScreenDrag:
-			var dir = event.position - ptr_pos_at_Rest - ptr_size / 2
-			ptr_pos = min(dir.length(),100) * dir.normalized()
-			
-			_calc_Component()
-			emit_signal("Move", FRONT, LEFT)
-		else:
-			FRONT = 0
-			LEFT = 0
-
 func _on_Pointer_gui_input(event):
-	_on_gui(event)
+#	_on_gui(event)
+	out = false
 	
 	if event is InputEventScreenTouch:
-		if not run:
-			if onHit :
-				ptr_pos = event.position - ptr_size / 2
-				_calc_Component()
-				emit_signal("Move", FRONT, LEFT)
-	elif event is InputEventMouseMotion:
+#		if not run:
+		ptr_pos = event.position - ptr_size / 2
+		_calc_Component()
+		emit_signal("Move", FRONT, LEFT)
+	elif event is InputEventScreenDrag:
 		var delta = event.relative
+
+		ptr_pos += delta
+		ptr_pos = min(ptr_pos.length(),100) * ptr_pos.normalized()
 		
-		if onHit:
-			ptr_pos += delta
-			ptr_pos = min(ptr_pos.length(),100) * ptr_pos.normalized()
-			
-			emit_signal("Move", FRONT, LEFT)
+		emit_signal("Move", FRONT, LEFT)
+	elif event is InputEventMouse:
+		
+	elif event is InputEventMouse:
+		
 	else:
 		out = true
 		FRONT = 0
