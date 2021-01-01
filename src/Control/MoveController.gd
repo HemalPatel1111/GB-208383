0545eb567384f1b0e018e1f9d7b452b78e43cba5
Hemal Patel <38472887+HemalPatel1111@users.noreply.github.com>
2021-01-01 15:45:45 +0530

    Touch test

--- src/Control/MoveController.gd
+++ src/Control/MoveController.gd
@@ -87,9 +87,9 @@ func _on_Move_gui_input(event):
 			if onHit:
 				if antiRotation:
 					_rotResistance = event.relative
-		if onHit:
-			var dir = event.position - ptr_pos_at_Rest - ptr_size / 2
-			ptr_pos = min(dir.length(),100) * dir.normalized()
+			if onHit:
+				var dir = event.position - ptr_pos_at_Rest - ptr_size / 2
+				ptr_pos = min(dir.length(),100) * dir.normalized()
 			
 			_calc_Component(true)
 		else:
---
+++ src/Control/MoveController1.gd
@@ -0,0 +1,162 @@
+extends TextureButton
+
+export var sensitivity:float = 0.01
+
+var ptr_pos_at_Rest:Vector2 = Vector2(65,65)
+var ptr_pos:Vector2 = Vector2()
+var ptr_size:Vector2 = Vector2(60,60)
+
+signal Move(speedFront, speedLeft)
+signal onHit()
+signal run(run)
+
+const RUN_WAIT:float = 2.0
+var runWait:float = RUN_WAIT
+
+var run:bool = false
+
+var FRONT :float = 0
+var oldFRONT :float = 0
+var LEFT  :float = 0
+var oldLEFT  :float = 0
+var onHit:bool=true
+
+func _process(delta):
+	var pos = $Pointer.get_position()
+	var deltaPos = ptr_pos_at_Rest - pos
+	
+	_calc_Component()
+	
+	if ptr_pos.y <= -90:
+		if runWait > 0:
+			runWait -= delta
+		else:
+			runWait = 0
+			run = true
+			emit_signal("run", run)
+	else:
+		runWait = RUN_WAIT
+		run = false
+		emit_signal("run", run)
+
+#	if not run:
+	if(!onHit):
+		$Pointer.set_position(pos + deltaPos / 5)
+		ptr_pos = pos - ptr_pos_at_Rest
+		if ptr_pos.length() > 0.1:
+			emit_signal("Move", FRONT, LEFT)
+	else:
+		$Pointer.set_position(ptr_pos_at_Rest + ptr_pos)
+#	else:
+#		if(onHit):
+#			$Pointer.set_position(ptr_pos_at_Rest + ptr_pos)
+
+func _calc_Component(signalIt:bool = false):
+	oldFRONT = FRONT
+	oldLEFT = LEFT
+	FRONT = ptr_pos.y * sensitivity
+	LEFT = ptr_pos.x * sensitivity
+	if signalIt:
+		emit_signal("Move", FRONT, LEFT)
+
+func _on_gui(event):
+#	if event is InputEventMouseButton:
+#		if event.button_mask == 1 :
+#			onHit = true
+#			emit_signal("onHit")
+#		else :
+#			onHit = false
+	pass
+
+func _on_Move_gui_input(event):
+	var i:int = 0
+#	_on_gui(event)
+#
+#	if not run:
+#		if _rotResistance.length() > 0:
+#			_rotResistance *= 0
+#
+#		if event is InputEventMouseButton:
+#			if onHit :
+#				var dir = event.position - ptr_pos_at_Rest - ptr_size / 2
+#				ptr_pos = min(dir.length(),100) * dir.normalized()
+#				_calc_Component(true)
+#		if event is InputEventMouseMotion:
+#			if onHit:
+#				if antiRotation:
+#					_rotResistance = event.relative
+#			if onHit:
+#				var dir = event.position - ptr_pos_at_Rest - ptr_size / 2
+#				ptr_pos = min(dir.length(),100) * dir.normalized()
+#
+#			_calc_Component(true)
+
+	if event is InputEventMouseButton:
+		pass
+#		if not run:
+#			if onHit :
+#				ptr_pos = event.position - ptr_size / 2
+#				_calc_Component(true)
+	elif event is InputEventMouseMotion:
+		pass
+#		var delta = event.relative
+#
+#		if onHit:
+#			ptr_pos += delta
+#			ptr_pos = min(ptr_pos.length(),100) * ptr_pos.normalized()
+#
+#			_calc_Component(true)
+	if event is InputEventScreenTouch:
+		var e:InputEventScreenTouch = event
+		if e.pressed:
+			ptr_pos = e.position - ptr_size / 2
+			_calc_Component(true)
+			i+=1
+	
+	if event is InputEventScreenDrag:
+		var e:InputEventScreenDrag = event
+		if $".".get_rect().has_point(e.position):
+			ptr_pos += e.relative
+			ptr_pos = min(ptr_pos.length(),100) * ptr_pos.normalized()
+			_calc_Component(true)
+			i+=1
+			#				var dir = event.position - ptr_pos_at_Rest - ptr_size / 2
+#				ptr_pos = min(dir.length(),100) * dir.normalized()
+#
+#			_calc_Component(true)
+	onHit = (i > 0)
+	pass
+
+func _on_Pointer_gui_input(event):
+	_on_gui(event)
+	var i:int = 0
+	if event is InputEventMouseButton:
+		pass
+#		if not run:
+#			if onHit :
+#				ptr_pos = event.position - ptr_size / 2
+#				_calc_Component(true)
+	elif event is InputEventMouseMotion:
+		pass
+#		var delta = event.relative
+#
+#		if onHit:
+#			ptr_pos += delta
+#			ptr_pos = min(ptr_pos.length(),100) * ptr_pos.normalized()
+#
+#			_calc_Component(true)
+	if event is InputEventScreenTouch:
+		var e:InputEventScreenTouch = event
+		if e.pressed:
+			ptr_pos = e.position - ptr_size / 2
+			_calc_Component(true)
+			i+=1
+	if event is InputEventScreenDrag:
+		var e:InputEventScreenDrag = event
+		if $".".get_rect().has_point(e.position):
+			ptr_pos += e.relative
+			ptr_pos = min(ptr_pos.length(),100) * ptr_pos.normalized()
+			_calc_Component(true)
+			i+=1
+			
+	onHit = (i > 0)
