8723c327c9edce6933af09b604e6c12ec2cface2
Hemal Patel <38472887+HemalPatel1111@users.noreply.github.com>
2021-01-06 23:25:02 +0530

    load

--- src/Control/GiftButton.gd
+++ src/Control/GiftButton.gd
@@ -2,21 +2,26 @@ extends Control
 class_name GiftButton
 
 var index:= -1
-var texture:=ImageTexture.new()
+var texture:=StreamTexture.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
+func _process(delta):
+	update()
+
 func load_gift(id:int, type_id:int):
 	GiftData.load_data()
 	var path:String= GiftData.gifts_icons[type_id - 1]
-	texture.load(path)
+	texture = load(path)
 	index = id - 1
-
-func _on_Label_ready():
+	$Icon.set_stretch_mode(5)
+	$Icon.set_expand(true)
+	$Icon.set_pressed_texture(texture)
 	$Label.text = "Gift " + str(index + 1)
 
+func _on_Label_ready():
+	pass
 func _on_Icon_ready():
-	$Icon.set_pressed_texture(texture)
 	pass
--- src/Menu/TouchTest.gd
+++ src/Menu/TouchTest.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var btn:GiftButton = null
+
 func _ready():
 	pass
 
@@ -8,9 +10,9 @@ func _on_Tree_ready():
 
 
 func _on_Tree_tree_entered():
-		for x in range(0,4):
-			var item = load("res://src/Control/GiftButton.tscn")
-			var btn:GiftButton = item.instance()
-			$Tree.add_child(btn)
-			btn.load_gift(x, 1)
-			btn.set_global_position(Vector2(0, 20 + x * 20))
+	for x in range(0,4):
+		var item = load("res://src/Control/GiftButton.tscn")
+		btn = item.instance()
+		$Tree.add_child(btn)
+		btn.load_gift(x, 1)
+		btn.set_global_position(Vector2(0, 20 + x * 20))
