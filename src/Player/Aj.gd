c69522935ffb59eeeb716e00104a406ba93fcb44
Hemal Patel <38472887+HemalPatel1111@users.noreply.github.com>
2021-01-08 13:17:44 +0530

    Players Added

--- src/Player/Aj.gd
+++ src/Player/Aj.gd
@@ -1,2 +1,22 @@
-extends Spatial
+extends Player
 
+
+func init():
+	if not loaded:
+		_animPlayer = $AnimationPlayer
+		_player = $"."
+		_playerCharacter = $Boy
+		_camera = $Camera
+		loaded = true
+		
+		rot = _camera.rotation
+		
+		trackerDirBase = _camera.translation
+		trackerTranslation.y = trackerDirBase.y
+		trackerDirBase -= trackerTranslation
+		trackerDist = trackerDirBase.length()
+		
+		trackerDirBase = trackerDirBase.normalized()
+		trackerDir = trackerDirBase
+		
+		scale = Vector3(PLAYER_SCALE, PLAYER_SCALE, PLAYER_SCALE)
--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -17,6 +17,49 @@ var cur_anim:String
 var AnimationList:PoolStringArray
 var loop:bool = false
 
+var loaded:bool = false
+
+
+var run:bool = false
+
+var UP:Vector3 = Vector3(0,1,0) #_camera UP direction
+var Look:Vector3 = Vector3() 	#Camera Look direction
+var Left:Vector3 = Vector3() 	#Camera Left direction
+
+var rot:Vector3 = Vector3() 	#Camera Target rotation
+
+var velocityp:Vector3 = Vector3() 	#Player Velocity frm Move Controller
+
+var trackerTranslation:Vector3 = Vector3() 	#Position of player Camera
+var trackerDirBase:Vector3 = Vector3() 	#Position of player Camera
+var trackerDir:Vector3 = Vector3() 	#Position of player Camera 
+var trackerDist:float = 10.0
+
+var move_dir:Vector3 = Vector3()	#direction of movement of player
+
+export var PLAYER_SCALE:float = 0.2 					#Scalling of the player
+var PLAYER_WALK	:float = 2.4642 * PLAYER_SCALE #player walk length per walk animation
+var PLAYER_RUN	:float = 9.1094 * PLAYER_SCALE #player walk length per walk animation
+var PLAYER_WALK_PISTOL:float = 4.1693 * PLAYER_SCALE
+var PLAYER_RUN_PISTOL:float = 4.9035 * PLAYER_SCALE
+var PLAYER_WALK_RIFLE:float = 1.5812 * PLAYER_SCALE
+var PLAYER_RUN_RIFLE:float = 4.9166 * PLAYER_SCALE
+
+var player_walk:float = PLAYER_WALK
+var player_run:float = PLAYER_RUN
+
+export var mouse_sens			:float = 0.01					#Mouse sensitivity
+export var rotation_multiplier	:float = 25.0
+var frameTime	:float = 0.0					#Time since last frame
+
+var _animPlayer:AnimationPlayer
+var _player			:Player			#the player root
+var _playerCharacter :KinematicBody	#the player Character
+var _camera			:Camera			
+
+func set_run(running:bool):
+	run = running
+
 func set_idle_id(id:int):
 	idle_id = id
 	
@@ -29,56 +72,146 @@ func set_run_id(id:int):
 func set_fire_id(id:int):
 	fire_id = id
 
-func set_weapon(weapon:int) :
+func init():
 	pass
+	
+func set_weapon(weapon:int):
+	match weapon:
+		Weapon.HAND:
+			idle_id = 0 ; walk_id = 1; run_id = 2; fire_id = 3
+			player_walk = PLAYER_WALK
+			player_run = PLAYER_RUN
+		Weapon.PISTOL:
+			idle_id = 0; walk_id = 1; run_id = 2; fire_id = 3
+			player_walk = PLAYER_WALK_PISTOL
+			player_run = PLAYER_RUN_PISTOL
+		Weapon.RIFLE:
+			idle_id = 0; walk_id = 1; run_id = 2; fire_id = 3
+			player_walk = PLAYER_WALK_RIFLE
+			player_run = PLAYER_RUN_RIFLE
+
+func update(delta):
+	frameTime = delta
+	#rot = camera.rotation
+	
+	Look = Vector3(0,0,1)
+	UP = Vector3(0,1,0)
+	Left = Vector3(1,0,0)
+	
+	Look = Look.rotated(UP, rot.y)
+	Left = Left.rotated(UP, rot.y)
+	
+	trackerDir = trackerDirBase.rotated(UP, rot.y)
+	trackerDir = -trackerDir.rotated(Left, rot.x)
+	
+	if move_dir.length() > 0.1:
+		_playerCharacter.rotation.y = _camera.rotation.y + atan2(move_dir.z,-move_dir.x) - PI/2;
+	
+	velocityp = velocityp * 0
+	
+	if fire:
+		_fire()
+	else:
+		if move_dir.length() > 0.1:
+				if run:
+					_run()
+				else:
+					_walk()
+		else:
+			if idle_id >= 0:
+				animate_id(idle_id)
+			else:
+				animate(IdleBreath)
+	
+	velocityp = _playerCharacter.move_and_slide(velocityp,UP)
+	
+	_camera.translation = _playerCharacter.translation
+	_camera.translation += trackerTranslation.y * UP
+	_camera.translation += trackerDir * trackerDist
+
+func update_rotate(dir:Vector2):
+	rot.y += -dir.x * mouse_sens * frameTime * rotation_multiplier
+	rot.x += -dir.y * mouse_sens * frameTime * rotation_multiplier
+	rot.x = -min(max(-rot.x,-PI/8),PI/2)
+	_camera.rotation = rot
+
+func set_move(speedFront, speedLeft):
+	move_dir.x = speedLeft;  move_dir.z = speedFront
+	if move_dir.length() > 0.1:
+		move_dir = move_dir.normalized() * max(move_dir.length(), 0.7)
+		
+func loadAnimationList() -> PoolStringArray:
+	_loadAnimationList(_animPlayer)
+	return AnimationList
+
+func is_on_floor() -> bool:
+	return _playerCharacter.is_on_floor()
+	
+func move_and_slide(velocityg:Vector3, _Up:Vector3) -> Vector3:
+	return _playerCharacter.move_and_slide(velocityg,_Up)
+	
+func animate_id(id:int, forward:bool = true):
+	var name:String = AnimationList[id]
+	play_animation(name, forward)
+
+func animate(name:String, forward:bool = true):
+	play_animation(name, forward)
+
+func _fire():
+	if fire_id >= 0 : animate_id(fire_id)
+
+func _walk():
+	if walk_id >= 0:
+		animate_id(walk_id)
+	else:
+		play_animation(Walk, true, sqrt(abs(move_dir.length())))
+
+	velocityp = move_dir.x * Left * player_walk
+	velocityp += move_dir.z * Look * player_walk
+
+func _run():
+	if run_id >= 0:
+		animate_id(run_id)
+	else:
+		play_animation(Run, true, sqrt(abs(move_dir.length())))
+	
+	velocityp = move_dir.x * Left * player_run
+	velocityp += move_dir.z * Look * player_run
+
+func get_animation(x:String) -> Animation:
+	return _animPlayer.get_animation(x)
+
+func play_animation_id(id:int, forward:bool = true, speed:float = 1.0):
+	_animPlayer.set_speed_scale(speed)
+	
+	animate_id(id, forward)
+
+func play_animation(name:String, forward:bool = true, speed:float = 1.0):
+	_animPlayer.set_speed_scale(speed)
+	
+	if not _animPlayer.get_current_animation() == name:
+		if forward:
+			_animPlayer.play(name)
+		else:
+			_animPlayer.play_backwards(name)
+	else:
+		if forward:
+			if backward:
+				_animPlayer.play(name)
+		else:
+			if not backward:
+				_animPlayer.play_backwards(name)
+	backward = not forward
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	fireIt(false)
+
 
 func fireIt(value:bool):
 	fire = value
 
-func move_and_slide(velocityg:Vector3, UP:Vector3) -> Vector3:
-	return Vector3()
-
-func is_on_floor() -> bool:
-	return false
-
-func init():
-	pass
-
-func set_run(run:bool):
-	pass
-
-func update(delta):
-	pass
-
-func update_rotate(dir:Vector2):
-	pass
-
-func set_move(speedFront, speedLeft):
-	pass
-# Loads Animation list
-func loadAnimationList() -> PoolStringArray:
-	return PoolStringArray()
-	
-# Animates the character
-func animate_id(id:int, forward:bool = true):
-	pass
-	
-func animate(name:String, forward:bool = true):
-	pass
-
-# plays a particular animation
-func play_animation_id(id:int, forward:bool = true, speed:float = 1.0):
-	pass
-
-func play_animation(name:String, forward:bool = true, speed:float = 1.0):
-	pass
-# gets a particular animation
-
-func get_animation(x:String) -> Animation:
-	return null
-# generates the Animation List
-
 #---------------- Protected Function -------------------------------
 
-func _loadAnimationList(animPlayer:AnimationPlayer):
-	AnimationList = animPlayer.get_animation_list()
+func _loadAnimationList(_animPlayer:AnimationPlayer):
+	AnimationList = _animPlayer.get_animation_list()
