14bada5069ba5e33a8e23934c9db6bc880e71e3d
HemalPatel1111 <38472887+HemalPatel1111@users.noreply.github.com>
2021-01-09 13:58:28 +0530

    level sceme

--- src/Scenes/Level1.gd
+++ src/Scenes/Level1.gd
@@ -10,3 +10,15 @@ func _ready():
 	
 	gifts_type.append(1)
 	gifts_target.append(1)
+	
+	gifts_type.append(1)
+	gifts_target.append(1)
+	
+	gifts_type.append(1)
+	gifts_target.append(1)
+	
+	gifts_type.append(1)
+	gifts_target.append(1)
+	
+	gifts_type.append(1)
+	gifts_target.append(1)
